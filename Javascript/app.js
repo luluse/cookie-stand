5be614e173e5053a42c02f934976f3d958a20352
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-05-04 14:02:19 -0700

    add object literals for each store

---
+++ Javascript/app.js
@@ -0,0 +1,29 @@
+var seattle = {
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgCookie: 6.3
+};
+
+var tokyo = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookie: 1.2
+};
+
+var dubai = {
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookie: 3.7
+};
+
+var paris = {
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCookie: 2.3
+};
+
+var lima = {
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgCookie: 4.6
+};
