2825657ed0f69bd7182f8e2238f90078149b3f7d
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-05-04 20:56:44 -0700

    solve lima

--- Javascript/app.js
+++ Javascript/app.js
@@ -146,30 +146,38 @@ paris.randomCustomer();
 paris.avgCookiePerHour();
 paris.render();
 
+
 var lima = {
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   minCustomer: 2,
   maxCustomer: 16,
-  avgCookie: 4.6
+  avgCookiePerCustomer: 4.6,
+  cookieSales: [],
+  randomCustomer: function(){
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1)) + this.minCustomer;
+  },
+  avgCookiePerHour: function() {
+    for (var i = 0; i < this.hoursOfOperation.length; i++){
+      this.cookieSales.push(Math.floor(this.avgCookiePerCustomer * this.randomCustomer()));
+    }
+  },
+  render: function(){
+    var parent = document.getElementById('lima');
+    for (var i = 0; i < this.hoursOfOperation.length; i++ ){
+      var listItem = document.createElement('li');
+      listItem.textContent = `${this.hoursOfOperation[i]}: ${this.cookieSales[i]}`;
+      parent.appendChild(listItem);
+    }
+    var totalCookies = 0;
+    for (var j = 0; j < this.cookieSales.length; j++){
+      totalCookies += this.cookieSales[j];
+    }
+    var totalItem = document.createElement('li');
+    totalItem.textContent = `Total: ${totalCookies}`;
+    parent.appendChild(totalItem);
+  }
 };
 
-//   randomCustomer: function() {
-//     for (var i = 0; i < this.hoursOfOperation.length; i++) {
-//       return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1)) + this.minCustomer;
-//     }
-//     var avgCustomer = [],
-//     avgCustomer.push(seattle.randomCustomer),
-//   },
-//   avgCookiePerHour: function() {
-//     for (var i = 0; i < this.hoursOfOperation.length; i++) {
-//       return this.randomCustomer[i] * this.avgCookiePerCustomer;
-//     }
-//   }
-//   }
-// };
-
-
-// generateRandomCustPerHour: function() {
-//   for(var i = 0; i < this.hoursOfOps.length; i++) {
-//     var randomCust = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
-//     this.custPerHour.push(randomCust);
-// }
+lima.randomCustomer();
+lima.avgCookiePerHour();
+lima.render();
