5757091bae86a3eea1de7a5c2584e1c0e572f5eb
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-05-04 17:20:21 -0700

    seattle solved

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,8 +1,59 @@
+
+// var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+
 var seattle = {
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   minCustomer: 23,
   maxCustomer: 65,
-  avgCookie: 6.3
+  avgCookiePerCustomer: 6.3,
+  cookieSales: [],
+  randomCustomer: function() {
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1)) + this.minCustomer;
+  },
+  avgCookiePerHour: function() {
+    for (var i = 0; i < this.hoursOfOperation.length; i++){
+      this.cookieSales.push(Math.floor(this.avgCookiePerCustomer * this.randomCustomer()));
+    }
+  },
+  render: function() {
+    var parent = document.getElementById('seattle');
+    for (var i = 0; i < this.hoursOfOperation.length; i++){
+      var listItem = document.createElement('li');
+      listItem.textContent = `${this.hoursOfOperation[i]} ${this.cookieSales[i]}`;
+      parent.appendChild(listItem);
+    }
+  }
 };
+
+seattle.randomCustomer();
+seattle.avgCookiePerHour();
+seattle.render();
+
+
+//   randomCustomer: function() {
+//     for (var i = 0; i < this.hoursOfOperation.length; i++) {
+//       return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1)) + this.minCustomer;
+//     }
+//     var avgCustomer = [],
+//     avgCustomer.push(seattle.randomCustomer),
+//   },
+//   avgCookiePerHour: function() {
+//     for (var i = 0; i < this.hoursOfOperation.length; i++) {
+//       return this.randomCustomer[i] * this.avgCookiePerCustomer;
+//     }
+//   }
+//   }
+// };
+
+// console.log(seattle.randomCustomer[i]);
+
+
+// generateRandomCustPerHour: function() {
+//   for(var i = 0; i < this.hoursOfOps.length; i++) {
+//     var randomCust = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
+//     this.custPerHour.push(randomCust);
+//   }
 
 var tokyo = {
   minCustomer: 3,
